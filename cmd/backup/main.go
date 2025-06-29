9bd7a58bcc502b5f0130724d894d3575a5bdf7e1
simone viozzi <simoneviozzi97@gmail.com>
2025-06-29 18:29:57 +0200

    Add config style flag

--- cmd/backup/main.go
+++ cmd/backup/main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	foreground := flag.Bool("foreground", false, "run the tool in the foreground")
 	profile := flag.String("profile", "", "collect runtime metrics and log them periodically on the given cron expression")
+	configStyle := flag.String("config-style", "envfile", "configuration style: envfile or labels")
 	flag.Parse()
 
 	c := newCommand()
@@ -17,8 +18,8 @@ func main() {
 		opts := foregroundOpts{
 			profileCronExpression: *profile,
 		}
-		c.must(c.runInForeground(opts))
+		c.must(c.runInForeground(opts, *configStyle))
 	} else {
-		c.must(c.runAsCommand())
+		c.must(c.runAsCommand(*configStyle))
 	}
 }
